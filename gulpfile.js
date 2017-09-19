7884b7c96ddc417b79a717a6a44b63bfd31515fe
Zack Dobbelaar <dobbelaar@gmail.com>
2017-09-19 15:39:50 -0400

    init setup

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+// generated on <%= date %> using <%= name %> <%= version %>
+
+const gulp = require('gulp');
+const gulpLoadPlugins = require('gulp-load-plugins');
+const del = require('del');
+const $ = gulpLoadPlugins();
+var gp_concat = require('gulp-concat-util'),
+gp_rename = require('gulp-rename'),
+htmlreplace = require('gulp-html-replace'),
+replace = require('gulp-replace-task'),
+deleteLines = require('gulp-delete-lines'),
+runSequence = require('run-sequence'),
+removeEmptyLines = require('gulp-remove-empty-lines'),
+fs = require('fs');
+
+
+gulp.task('transmute', function(done) {
+        console.log('Transmute Complete....');
+});
